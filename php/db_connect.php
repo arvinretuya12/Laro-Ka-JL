96be8ffd37d62a7c019c8dd0fa1f887f21d8c028
Arvin Dev <arvinretuya12@gmail.com>
2026-07-24 17:00:04 +0800

    Updated DB Password

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $user = 'root'; // Default XAMPP user
-$pass = ''; // Default XAMPP password is empty
+$pass = 'joloverseadmin1234'; // Default XAMPP password is empty
 $db = 'laro_event_db';
 
 $conn = new mysqli($host, $user, $pass, $db);
